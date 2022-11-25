50f968c33dbe47ed8afb120dc3f9288f1a804c6b
Petros Laskas <laskasp15@gmail.com>
2022-11-25 19:12:36 +0200

    Refactor init

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
+const navToggle = document.querySelector(".navbar_toggle");
+const links = document.querySelector(".main_nav");
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-
-        // Add a click event on each of them
-        $navbarBurgers.forEach(el => {
-            el.addEventListener('click', () => {
-
-                // Get the target from the "data-target" attribute
-                const target = el.dataset.target;
-                const $target = document.getElementById(target);
-
-                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-                el.classList.toggle('is-active');
-                $target.classList.toggle('is-active');
-
-            });
-        });
-    }
-
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show_nav");
 });
